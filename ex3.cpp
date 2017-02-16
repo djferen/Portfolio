859dec2af9b2fae027342f06d4f591092ab9cfff
Daniel Feren <djferen@gmail.com>
2017-02-15 23:58:03 -0500

    EX3 Added linked list methods

--- ex3.cpp
+++ ex3.cpp
@@ -1,16 +1,60 @@
 #include <cstddef>
+#include <iostream>
 
 struct Node {
      int val;
      Node *next;
      Node(int x) : val(x), next(NULL) {}
      Node(): val(0), next(NULL){}
- }; 
+};
 
+struct LinkedList {
+
+  Node* head;
+  LinkedList(){head = NULL;}
+
+void push(int data)
+{
+  Node * newnode = new Node(data);
+  newnode->next = head;
+  head = newnode;
+}
+
+void print()
+{
+  Node * iter = head;
+  while(iter)
+  {
+    std::cout << iter->val << std::endl; 
+    iter = iter->next;
+  }
+}
+
+};
+
+void print(Node * a)
+{
+  Node * iter = a;
+  while(iter)
+  {
+    std::cout << iter->val << " "; 
+    iter = iter->next;
+  }
+  std::cout << std::endl;
+}
 
 int main ()
 {
   Node * newnode = new Node();
+
+  LinkedList a,b;
+  a.push(1);
+  a.push(2);
+  a.push(2);
+  a.print();
+
+  LinkedList c;
+  add(a.head, b.head);
 
   return 0;
 }
