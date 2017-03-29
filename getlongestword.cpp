170f848358d2e4eff9fed1a915c647ad08b1dc9a
Daniel Feren <djferen@gmail.com>
2017-03-28 21:32:39 -0700

    Completes implementation of getLongestWord

--- getlongestword.cpp
+++ getlongestword.cpp
@@ -4,86 +4,86 @@
 
 using namespace std;
 
-vector<string> GetWords(string s, char delim)
+vector < string > GetWords (string s, char delim)
 {
-	vector<string> words;
-	if(s.find(delim) == string::npos)
-	{
-		words.push_back(s);
-	    return words;
-	}
-	else
-	{
-		stringstream ss(s);
-		string word;
-		while( getline(ss, word, delim) )
-		{
-			words.push_back(word);
-		}
+  vector < string > words;
+  if (s.find (delim) == string::npos) {
+    words.push_back (s);
+    return words;
+  }
+  else {
+    stringstream ss (s);
+    string word;
+    while (getline (ss, word, delim)) {
+      words.push_back (word);
+    }
 
-	    return words;
-	}
+    return words;
+  }
 }
 
-string LongestWord(string sen)
+string
+LongestWord (string sen)
 {
-  if(sen == "")
-	  return "";
+  if (sen == "")
+    return "";
 
-  if(sen[0] == ' ')
-	  sen = sen.substr(1);
+  if (sen[0] == ' ')
+    sen = sen.substr (1);
 
-  if(sen[sen.length()-1] == ' ')
-	  sen = sen.substr(0, sen.length()-2);
+  if (sen[sen.length () - 1] == ' ')
+    sen = sen.substr (0, sen.length () - 1);
 
-  vector<string> words = GetWords(sen, ' ');
-  
+  vector < string > words = GetWords (sen, ' ');
+
   size_t index = 0;
   int longestWord = 0;
   bool found = false;
-  
-  for(int i = words.size()-1; i > 0; i--)
-  {
-      int length = words[i].length();
-      if( length >= longestWord)
-      {
-          longestWord = length;
-          index = i;
-          found = true;
-      }
+
+  for (int i = words.size () - 1; i >= 0; i--) {
+    int length = words[i].length ();
+    if (length >= longestWord) {
+      longestWord = length;
+      index = i;
+      found = true;
+    }
   }
-  
-  return (found ? words[index] : ""); 
+
+  return (found ? words[index] : "");
 }
 
-int main()
+int
+main ()
 {
-  size_t tests          = 4;
-  string test[tests][2] = {{"",""},
-                           {"A","A"},
-		                   {" A ","A"},
-                           {"A AB", "AB"}};
+  size_t tests = 7;
+  string test[tests][2] = { {"", ""},
+  {"        ", ""},
+  {"A", "A"},
+  {" A ", "A"},
+  {"A AB", "AB"},
+  {"AA AA A B", "AA"},
+  {"%!#!@ ###3###", "###3###"}
+  };
   string input;
   string actual;
   string expected;
 
-  for(size_t i = 0; i < tests; i++)
-  {
-	  input = test[i][0];
-	  actual = LongestWord(input);
-	  expected = test[i][1];
-	  cout << "Case #" << i << " input: '" << input << "' expected: '" << expected << "' actual: '" << actual << "'" << endl;
-	  if( actual != expected )
-	  {
-		  cout << actual << " did not match " << expected << "; Test failed!" << endl;
-	  }
-	  else
-	  {
-		  cout << actual << " did match " << expected << ": Test passed!" << endl;
-	  }
-	  cout << endl;
+  for (size_t i = 0; i < tests; i++) {
+    input = test[i][0];
+    actual = LongestWord (input);
+    expected = test[i][1];
+    cout << "Case #" << i +
+      1 << " input: '" << input << "' expected: '" << expected <<
+      "' actual: '" << actual << "'" << endl;
+    if (actual != expected) {
+      cout << actual << " did not match " << expected << "; Test failed!" <<
+	endl;
+    }
+    else {
+      cout << actual << " did match " << expected << ": Test passed!" << endl;
+    }
+    cout << endl;
   }
 
   return 0;
-} 
-
+}
