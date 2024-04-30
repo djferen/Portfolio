acbdcc603faca5ddf1b54b92a264d8f0317f36ae
Daniel Feren <3378646+djferen@users.noreply.github.com>
2024-04-30 23:25:48 +0000

    Sorry, wrong operator

--- test/test-poly.cpp
+++ test/test-poly.cpp
@@ -18,13 +18,13 @@ int main() {
   Creator* creator1 = new ConcreteCreator1();
   ClientCode(*creator1);
   Product * product1 = creator1->FactoryMethod();
-  cout << endl << product1.Operation() << endl;
+  cout << endl << product1->Operation() << endl;
 
   cout << endl << "App: Launched with the ConcreteCreator2.\n";
   Creator* creator2 = new ConcreteCreator2();
   ClientCode(*creator2);
   Product * product2 = creator2->FactoryMethod();
-  cout << endl << product2.Operation() << endl;
+  cout << endl << product2->Operation() << endl;
 
   delete creator1;
   delete creator2;
