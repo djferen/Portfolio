e8dda6bfb6c5c1b01897a3f0fbeef0e8f96c4900
Daniel Feren <3378646+djferen@users.noreply.github.com>
2024-05-01 00:14:34 +0000

    Could create outside too

--- include/poly.h
+++ include/poly.h
@@ -59,66 +59,64 @@ private:
 
 // An interface
 class B {
-  public:
-    virtual ~B() {}
-    virtual void Display() = 0;
-    virtual void Action() = 0;  
+public:
+  virtual ~B() {}
+  virtual void Display() = 0;
+  virtual void Action() = 0;
 };
 
 class D : public B {
-  public:
-    virtual ~D() {}
-    void Display() {}
-    void Action() {}
+public:
+  virtual ~D() {}
+  void Display() {}
+  void Action() {}
 };
 
 class Product {
- public:
+public:
   virtual ~Product() {}
   virtual std::string Operation() const = 0;
 };
 
 class ConcreteProduct1 : public Product {
- public:
+public:
   std::string Operation() const override {
     return "{Result of the ConcreteProduct1}";
   }
 };
 class ConcreteProduct2 : public Product {
- public:
+public:
   std::string Operation() const override {
     return "{Result of the ConcreteProduct2}";
   }
 };
 
 class Creator {
- public:
-  virtual ~Creator(){};
-  virtual Product* FactoryMethod() const = 0;
+public:
+  virtual ~Creator() {};
+  virtual Product *FactoryMethod() const = 0;
   std::string SomeOperation() const {
-    Product* product = this->FactoryMethod();
-    std::string result = "Creator: The same creator's code has just worked with " + product->Operation();
+    Product *product = this->FactoryMethod();
+    std::string result =
+        "Creator: The same creator's code has just worked with " +
+        product->Operation();
     delete product;
     return result;
   }
 };
 
 class ConcreteCreator1 : public Creator {
- public:
-  Product* FactoryMethod() const override {
-    return new ConcreteProduct1();
-  }
+public:
+  Product *FactoryMethod() const override { return new ConcreteProduct1(); }
 };
 
 class ConcreteCreator2 : public Creator {
- public:
-  Product* FactoryMethod() const override {
-    return new ConcreteProduct2();
-  }
+public:
+  Product *FactoryMethod() const override { return new ConcreteProduct2(); }
 };
 
-void ClientCode(const Creator& creator) {
-  std::cout << "Client: I'm not aware of the creator's class, but it still works.\n"
-            << creator.SomeOperation() << std::endl;
+void ClientCode(const Creator &creator) {
+  std::cout
+      << "Client: I'm not aware of the creator's class, but it still works.\n"
+      << creator.SomeOperation() << std::endl;
 }
-
--- test/test-poly.cpp
+++ test/test-poly.cpp
@@ -7,7 +7,7 @@ int main() {
   ptrUser->Action();
   delete ptrUser;
 
-  Base * base = new Derived1;
+  Base *base = new Derived1;
   base->DisplayAction();
 
   B *b = new D;
@@ -15,15 +15,15 @@ int main() {
   b->Action();
 
   cout << "App: Launched with the ConcreteCreator1.\n";
-  Creator* creator1 = new ConcreteCreator1();
+  Creator *creator1 = new ConcreteCreator1();
   ClientCode(*creator1);
-  Product * product1 = creator1->FactoryMethod();
+  Product *product1 = creator1->FactoryMethod();
   cout << endl << product1->Operation() << endl;
 
   cout << endl << "App: Launched with the ConcreteCreator2.\n";
-  Creator* creator2 = new ConcreteCreator2();
+  Creator *creator2 = new ConcreteCreator2();
   ClientCode(*creator2);
-  Product * product2 = creator2->FactoryMethod();
+  Product *product2 = creator2->FactoryMethod();
   cout << endl << product2->Operation() << endl;
 
   delete creator1;
