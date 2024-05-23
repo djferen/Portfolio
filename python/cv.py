e299e5d18b4f81d37f9bd50e4509c5467f0a13d7
Daniel Feren <3378646+djferen@users.noreply.github.com>
2024-05-23 17:28:32 +0000

    Use 3 channels

--- python/cv.py
+++ python/cv.py
@@ -3,17 +3,13 @@ import random
 
 img = cv2.imread('test/test.jpg')
 
-print(img)
-
-print(img.shape)
-
-# img is numpy array, HxWxChannels
-
-cv2.imwrite('test/new1.jpg', img)
-
 tag = img[1:20, 1:200]
 img[21:40, 101:300] = tag
 
-cv2.imwrite('test/new2.jpg', img)
+cv2.imwrite('test/new1.jpg', tag)
 
-cv2.imwrite('test/new3.jpg', tag)
+for i in range(40):
+    for j in range(40):
+        img[i][j] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+
+cv2.imwrite('test/new2.jpg', img)
